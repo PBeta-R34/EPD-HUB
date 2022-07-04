3ba3714b2c21bc38f6a02324d9d89d9abcf99e7f
unknown <102679183+PBeta-R34@users.noreply.github.com>
2022-07-04 08:53:03 -0300

    some updates

--- Run.lua
+++ Run.lua
@@ -5,7 +5,7 @@ _G.EPD.HttpGet =
 	or (game.HttpGet 	and function(Link) return game:HttpGet(Link) end)
 	or error("No HTTP Get Function.")
 
-_G.EPD.DEBUG = true
+_G.EPD.DEBUG = true -- Enable For Debugging
 
 local LoadedFunction = loadstring(_G.EPD.HttpGet("https://raw.githubusercontent.com/PBeta-R34/EPD-HUB/main/Source/Loader.lua"))
 if type(LoadedFunction) == "function" then
