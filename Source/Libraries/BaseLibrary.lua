921b8bfd78a756b122d49ddfcfe239945b2f39e4
unknown <102679183+PBeta-R34@users.noreply.github.com>
2022-07-19 01:45:23 -0300

    BaseLibrary Upgrades

--- Source/Libraries/BaseLibrary.lua
+++ Source/Libraries/BaseLibrary.lua
@@ -1,6 +1,7 @@
 local BaseLib = {
 	-- Should Be Initialized By The Loader, But In Case It Isn't
 	RootPath = "https://raw.githubusercontent.com/PBeta-R34/EPD-HUB/main/Source/",
+	RelativePath = "",
 	Exploit = {
 		Name = "",
 		Checksums = {}
@@ -19,19 +20,78 @@ BaseLib.SafeLoad = function(Source)
 	return false
 end
 
+----------------------------------
+-- EXPLOIT API UNIVERSALIZATION --
+----------------------------------
+
+----------------
+-- NETWORKING --
+----------------
+
 BaseLib.HttpGet =
-	httpget or http_get
+	HttpGet or http_get
 	or (syn 			and function(Link) return syn.request({Url = Link, Method = "GET"}).Body end)
 	or (game.HttpGet	and function(Link) return game:HttpGet(Link) end)
 	or error("No HTTP Get Function.")
 
-BaseLib.ImportFile = function(FilePath)
-	return BaseLib.HttpGet(BaseLib.RootPath .. FilePath)
+BaseLib.HttpPost =
+	HttpPost or http_post
+	or (syn 			and function(Link, Body) return syn.request({Url = Link, Body = Body, Method = "POST"}).Body end)
+	or (game.HttpPost	and function(Link, Body) return game:HttpPost(Link, Body) end)
+	or error("No HTTP Post Function.")
+
+
+BaseLib.HttpRequest =
+	http_request
+	or (Syn 			and function(Request) return syn.request(Request) end)
+	
+
+-----------------
+-- FILE SYSTEM --
+-----------------
+
+BaseLib.ReadFile = 
+	readfile
+
+BaseLib.WriteFile = 
+	writefile
+
+BaseLib.AppendFile = 
+	writefile
+	or function(FileName, NewInfo) BaseLib.WriteFile(FileName, BaseLib.ReadFile(FileName) .. NewInfo) end
+
+BaseLib.IsFile = 
+	isfile
+
+
+BaseLib.CreateFolder = 
+	makefolder
+
+BaseLib.DeleteFolder = 
+	delfolder
+
+BaseLib.IsFolder = 
+	isfolder
+
+
+
+
+BaseLib.SetRelativePath = function(Relative)
+	if Relative:sub(#Relative, #Relative) ~= "//" then
+		Relative ..= "//"
+	end
+	BaseLib.RelativePath = BaseLib.RootPath .. Relative
 end
 
-BaseLib.Import = function(ModulePath)
-	return BaseLib.SafeLoad(BaseLib.ImportFile(ModulePath .. ".lua"))
+BaseLib.ImportFile = function(FilePath, UseRelative)
+	return BaseLib.HttpGet(((UseRelative and BaseLib.RelativePath) or BaseLib.RootPath) .. FilePath)
 end
+
+BaseLib.Import = function(ModulePath, UseRelative)
+	return BaseLib.SafeLoad(BaseLib.ImportFile(ModulePath .. ".lua", UseRelative))
+end
+
+
 
 
 
