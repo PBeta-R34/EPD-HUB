417b3aba2210ad834a5b4acb25eec45e678d508f
unknown <102679183+PBeta-R34@users.noreply.github.com>
2022-07-21 19:28:56 -0300

    Added GEnv

--- Run.lua
+++ Run.lua
@@ -1,11 +1,13 @@
-_G.EPD = _G.EPD or {}
-_G.EPD.HttpGet =
+local GEnv = _G
+
+GEnv.EPD = GEnv.EPD or {}
+GEnv.EPD.HttpGet =
 	httpget or http_get
 	or (syn 			and function(Link) return syn.request({Url = Link, Method = "GET"}).Body end)
 	or (game.HttpGet 	and function(Link) return game:HttpGet(Link) end)
 	or error("No HTTP Get Function.")
 
-_G.EPD.DEBUG = true -- Enable For Debugging
+GEnv.EPD.DEBUG = true -- Enable For Debugging
 
 local LoadedFunction = loadstring(_G.EPD.HttpGet("https://raw.githubusercontent.com/PBeta-R34/EPD-HUB/main/Source/Loader.lua"))
 if type(LoadedFunction) == "function" then
